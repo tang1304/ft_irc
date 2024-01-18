1124a76b059b4de4f73853fa099a011db3735f2d
tang1304 <tanguy.gellon@gmail.com>
2024-01-18 16:38:55 +0100

    working on socketInit()

--- srcs/Server.cpp
+++ srcs/Server.cpp
@@ -6,7 +6,7 @@
 /*   By: tgellon <tgellon@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/11 16:03:01 by tgellon           #+#    #+#             */
-/*   Updated: 2024/01/18 14:01:12 by tgellon          ###   ########lyon.fr   */
+/*   Updated: 2024/01/18 16:36:25 by tgellon          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,5 +38,15 @@ void	Server::socketInit(){
 	_socketFd = socket(AF_INET, SOCK_STREAM, 0);
 	if (_socketFd < 0)
 		throw std::runtime_error("Error: Socket creation failed");
-	;
+	int	opt = setsockopt(_socketFd, SOL_SOCKET, SO_REUSEADDR, (char *)&opt, sizeof(opt));
+	if (opt < 0)
+		throw std::runtime_error("Error: setsokopt() failed");
+	servAddr.sin_family = AF_INET;
+	servAddr.sin_addr.s_addr = INADDR_ANY;
+	servAddr.sin_port = htons(_port);
+	if (bind(_socketFd, (struct sockaddr*)&servAddr, sizeof(servAddr)) < 0)
+		throw std::runtime_error("Error: bind() failed");
+	if (listen(_socketFd, SOMAXCONN) < 0)
+		throw std::runtime_error("Error: listen() failed");
+	_pollFds.push_back({_socketFd, POLLIN, 0});
 }
