b5b52e356be776e65edee24692223a25d582a75b
Roro <rolando.rebois@yahoo.fr>
2024-01-29 09:28:07 +0100

    join

--- incs/Channel.hpp
+++ incs/Channel.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Channel.hpp                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tgellon <tgellon@student.42lyon.fr>        +#+  +:+       +#+        */
+/*   By: rrebois <rrebois@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/11 16:15:06 by tgellon           #+#    #+#             */
-/*   Updated: 2024/01/19 11:16:15 by tgellon          ###   ########lyon.fr   */
+/*   Updated: 2024/01/29 08:58:46 by rrebois          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,8 +20,19 @@ class Client;
 
 class Channel{
 private:
+	std::string				_topic;
+	std::string				_password;
+	std::vector	<Client>	_usersJoin;
+	std::vector	<Client>	_chanop;
+	bool					_privated; // invite only or not / false
+	bool					_changeTopic; // false = anyone true = chanops only / false
+	// bool					_passOnOff; // on?
+	int						_connected; //number of users/chanops connected
 
 public:
+	void	setPrivateChan();
+	void	setChangeTopic();
+	void	giveChanopStatus();
 
 };
 
