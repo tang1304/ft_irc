3f50f2e4cc6287aebfbd542fa7ed80ccf54f6590
rrebois <rrebois@student.42lyon.fr>
2024-02-05 13:09:16 +0100

    INVITE cmd done

--- incs/rpl.hpp
+++ incs/rpl.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   rpl.hpp                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tgellon <tgellon@student.42lyon.fr>        +#+  +:+       +#+        */
+/*   By: rrebois <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/24 16:21:25 by tgellon           #+#    #+#             */
-/*   Updated: 2024/02/05 08:47:38 by tgellon          ###   ########lyon.fr   */
+/*   Updated: 2024/02/05 11:47:41 by rrebois          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@
 
 //	1
 # define RPL_WELCOME( nickname, user_id) (":" + SERVERNAME + " 001 " + nickname + \
-	" :Welcome to the" + SERVERNAME + " Network, " + user_id + "[!" + user_id + "@" + LOCALHOST + "]\r\n")
+	" :Welcome to the " + SERVERNAME + " Network, " + user_id + "[!" + user_id + "@" + LOCALHOST + "]\r\n")
 
 // 2
 # define RPL_YOURHOST(client) (":" + SERVERNAME + " 002 " + client + " :Your host is " + SERVERNAME \
@@ -34,40 +34,81 @@
 //5
 # define RPL_ISUPPORT(client, tokens) (":" + SERVERNAME + " 005 " + client + " " + tokens + "\r\n")
 
+// 332
+# define RPL_TOPIC(client, channel, topic) (":" + SERVERNAME + " 332 " + client + " " + channel + " :" + topic + "\r\n")
+
+//341
+# define RPL_INVITING(client, nick, channel) (":" + SERVERNAME + " 341 " + client + " invited " + nick + " to the channel " + channel + "\r\n")
+
+//403
+# define ERR_NOSUCHCHANNEL(client, channel) (":" + SERVERNAME + " 403 " + client + " " + channel + " :No such channel\r\n")
+
+//405
+# define ERR_TOOMANYCHANNELS(client, channel) (":" + SERVERNAME + " 405 " + client + " " + channel + " :You have joined too many channels\r\n")
+
 //407
-# define ERR_TOOMANYTARGETS(client) (":" + SERVERNAME + " 407 " + client + " :Duplicate recipients. No message delivered")
+# define ERR_TOOMANYTARGETS(client) (":" + SERVERNAME + " 407 " + client + " :Duplicate recipients. No message delivered\r\n")
 
 //411
 # define ERR_NORECIPIENT(client, command) (":" + SERVERNAME + " 411 " + client + " :No recipient given (" + command + ")\r\n")
 
 //412
-# define ERR_NOTEXTTOSEND(client) (":" + SERVERNAME + " 412 " + client + " :No text to send")
+# define ERR_NOTEXTTOSEND(client) (":" + SERVERNAME + " 412 " + client + " :No text to send\r\n")
+
+//421
+# define ERR_UNKNOWNCOMMAND(client, command) (":" + SERVERNAME + " 421 " + client + " " + command + " :Unknown command\r\n")
+
+//431
+# define ERR_NONICKNAMEGIVEN(client) (":" + SERVERNAME + " 431 " + client + " :No nickname given\r\n")
+
+//432
+# define ERR_ERRONEUSNICKNAME(client, nick) (":" + SERVERNAME + " 432 " + client + " " + nick + " :Erroneus nickname\r\n")
+
+//433
+# define ERR_NICKNAMEINUSE(client, nick) (":" + SERVERNAME + " 433 " + client + " " + nick + " :Nickname is already in use\r\n")
+
+//442
+# define ERR_NOTONCHANNEL(client, channel) (":" + SERVERNAME + " 442 " + client + " " + channel + " :You're not on that channel\r\n")
+
+//443
+# define ERR_USERONCHANNEL(client, nick, channel) (":" + SERVERNAME + " 443 " + client + " " + nick + " " + channel + " :is already on channel\r\n")
+
+// 461
+# define ERR_NEEDMOREPARAMS(client, cmd) (":" + SERVERNAME + " 461 " + client + " " + cmd + " :Not enough parameters\r\n")
+
+//462
+# define ERR_ALREADYREGISTERED(client) (":" + SERVERNAME + " 462 " + client + " :You may not reregister\r\n")
+
+//464
+# define ERR_PASSWDMISMATCH(client) (":" + SERVERNAME + " 464 " + client + " :Password incorrect\r\n")
+
+//471
+# define ERR_CHANNELISFULL(client, channel) (":" + SERVERNAME + " 471 " + client + " " + channel + " :Cannot join channel (+l)\r\n")
+
+//473
+# define ERR_INVITEONLYCHAN(client, channel) (":" + SERVERNAME + " 473 " + client + " " + channel + " :Cannot join channel (+i)\r\n")
+
+//474
+# define ERR_BANNEDFROMCHAN(client, channel) (":" + SERVERNAME + " 474 " + client + " " + channel + " :Cannot join channel (+b)\r\n")
+
+//475
+# define ERR_BADCHANNELKEY(client, channel) (":" + SERVERNAME + " 475 " + client + " " + channel + " :Cannot join channel (+k)\r\n")
+
+//476
+# define ERR_BADCHANMASK(channel) (":" + SERVERNAME + " 476 " + channel + " :Bad Channel Mask\r\n")
+
+//482
+# define ERR_CHANOPRIVSNEEDED(client, channel) (":" + SERVERNAME + " 482 " + client + " " + channel + " :You're not channel operator\r\n")
 
 # define ERROR(error) ("ERROR: " + error + "\r\n")
-# define ERR_NEEDMOREPARAMS(client, cmd) (client + " " + cmd + " :Not enough parameters\r\n")
-# define ERR_NEEDLESSPARAMS " :Too many parameters\n"
-# define ERR_UNKNOWNCOMMAND(client, command) (client + " " + command + " :Unknown command\r\n")
-# define ERR_PASSWDMISMATCH(client) (client + " :Password incorrect\r\n")
-# define ERR_ALREADYREGISTERED(client) (client + " :You may not reregister\r\n")
-# define ERR_NONICKNAMEGIVEN(client) (client + " :No nickname given\r\n")
-# define ERR_ERRONEUSNICKNAME(client, nick) (client + " " + nick + " :Erroneus nickname\r\n")
-# define ERR_NICKNAMEINUSE(client, nick) (client + " " + nick + " :Nickname is already in use\r\n")
-# define ERR_TOOMANYCHANNELS(client, channel) (client + " " + channel + " :You have joined too many channels\r\n")
-# define ERR_BADCHANNELKEY(client, channel) (client + " " + channel + " :Cannot join channel (+k)\r\n")
-# define ERR_BANNEDFROMCHAN(client, channel) (client + " " + channel + " :Cannot join channel (+b)\r\n")
-# define ERR_CHANNELISFULL(client, channel) (client + " " + channel + " :Cannot join channel (+l)\r\n")
-# define ERR_INVITEONLYCHAN(client, channel) (client + " " + channel + " :Cannot join channel (+i)\r\n")
-# define ERR_BADCHANMASK(channel) (channel + " :Bad Channel Mask\r\n")
-# define ERR_NOSUCHCHANNEL(client, channel) (client + " " + channel + " :No such channel\r\n")
-# define ERR_NOTONCHANNEL(client, channel) (client + " " + channel + " :You're not on that channel\r\n")
-# define ERR_CHANOPRIVSNEEDED(client, channel) (client + " " + channel + " :You're not channel operator\r\n")
-# define ERR_USERONCHANNEL(client, nick, channel) (client + " " + nick + " " + channel + " :is already on channel\r\n")
 
-# define ERR_PASSFIRST(client) (client + " :Must confirm password first\r\n")
-# define ERR_NICKFIRST(client) (client + " :Must set nickname first\r\n")
-# define ERR_NOTREGISTERED(client) (client + " :You may register first\r\n")
+
+# define ERR_NOEXISTINGUSER(client, nick) (":" + SERVERNAME +client + " " + nick + " :No existing user\r\n")
+# define ERR_PASSFIRST(client) (":" + SERVERNAME +client + " :Must confirm password first\r\n")
+# define ERR_NICKFIRST(client) (":" + SERVERNAME +client + " :Must set nickname first\r\n")
+# define ERR_NOTREGISTERED(client) (":" + SERVERNAME +client + " :You may register first\r\n")
 
 # define RPL_USERJOIN(client, channel) (client + " is joining the channel " + channel + "\r\n")
 # define RPL_USERLEFT(client, channel) (client + " has left the channel " + channel + "\r\n")
-# define RPL_TOPIC(client, channel, topic) (client + " " + channel + " :" + topic + "\r\n")
+
 #endif
--- srcs/invite.cpp
+++ srcs/invite.cpp
@@ -3,21 +3,22 @@
 /*                                                        :::      ::::::::   */
 /*   invite.cpp                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rrebois <rrebois@student.42lyon.fr>        +#+  +:+       +#+        */
+/*   By: rrebois <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/02 12:22:53 by rrebois           #+#    #+#             */
-/*   Updated: 2024/02/02 12:22:53 by rrebois          ###   ########lyon.fr   */
+/*   Updated: 2024/02/05 11:46:56 by rrebois          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../incs/irc.hpp"
 
-int	invite_cmd(int fd, vecStr &cmd, Server &serv) // Any user on chan can invite any other user? users and chanop? Only members of the channel are allowed to invite other users.
+int	invite_cmd(int fd, vecStr &cmd, Server &serv)
 {
 	Client		user = serv.getClient(fd);
 	itVecChan	itChan;
 	itVecClient	itClient;
 	itVecClient	itChanop;
+	itClientMap itClientList;
 
 	if (cmd.size() < 3)
 		return (serv.getClientMap()[fd].setBufferSend(ERR_NEEDMOREPARAMS(user.getNickName(), cmd[0])), 1);
@@ -33,26 +34,34 @@ int	invite_cmd(int fd, vecStr &cmd, Server &serv) // Any user on chan can invite
 		if (itClient->getNickName() == user.getNickName())
 			break ;
 	}
-	for (itChanop = itChan->getUsersJoin().begin(); itChanop != itChan->getUsersJoin().end(); itChanop++)
+	for (itChanop = itChan->getChanop().begin(); itChanop != itChan->getChanop().end(); itChanop++)
 	{
 		if (itChanop->getNickName() == user.getNickName())
 			break ;
 	}
-	// if ((itClient == getUsersJoin().end() && itChanop == getChanop().end()) && !itChan->getPrivated())
-	// 	return (serv.getClientMap()[fd].setBufferSend(ERR_NOTONCHANNEL(user.getNickName(), cmd[2])), 1);
-	// if (itClient != getUsersJoin().end() && itChan->getPrivated())
-	// 	return (serv.getClientMap()[fd].setBufferSend(ERR_CHANOPRIVSNEEDED(user.getNickName(), cmd[2])), 1);
-	for (itClient = itChan->getUsersJoin().begin(); itClient != itChan->getUsersJoin().end(); itClient++)
+	 if ((itClient == itChan->getUsersJoin().end() && itChanop == itChan->getChanop().end()) && !itChan->getPrivated())
+	 	return (serv.getClientMap()[fd].setBufferSend(ERR_NOTONCHANNEL(user.getNickName(), cmd[2])), 1);
+	 if (itClient != itChan->getUsersJoin().end() && itChan->getPrivated()) // a tester
+	 	return (serv.getClientMap()[fd].setBufferSend(ERR_CHANOPRIVSNEEDED(user.getNickName(), cmd[2])), 1);
+	 for (itClient = itChan->getUsersJoin().begin(); itClient != itChan->getUsersJoin().end(); itClient++)
 	{
 		if (itClient->getNickName() == cmd[1])
 			break ;
 	}
-	for (itChanop = itChan->getUsersJoin().begin(); itChanop != itChan->getUsersJoin().end(); itChanop++)
+	for (itChanop = itChan->getChanop().begin(); itChanop != itChan->getChanop().end(); itChanop++)
 	{
 		if (itChanop->getNickName() == cmd[1])
 			break ;
 	}
-	if (itClient->getNickName() == cmd[1] || itChanop->getNickName() == cmd[1])
+	if ((itClient != itChan->getUsersJoin().end() && itClient->getNickName() == cmd[1]) || \
+		(itChanop != itChan->getChanop().end() && itChanop->getNickName() == cmd[1]))
 		return (serv.getClientMap()[fd].setBufferSend(ERR_USERONCHANNEL(user.getNickName(), cmd[1], cmd[2])), 1);
+	for (itClientList = serv.getClientMap().begin(); itClientList != serv.getClientMap().end(); itClientList++)
+		if (itClientList->second.getNickName() == cmd[1])
+			break ;
+	if (itClientList == serv.getClientMap().end())
+		return (serv.getClientMap()[fd].setBufferSend(ERR_NOEXISTINGUSER(user.getNickName(), cmd[1])), 1);
+	sendToChan(*itChan, RPL_INVITING(user.getNickName(), cmd[1], itChan->getName()));
+	itChan->addUser(itClientList->second);
 	return (0);
 }
