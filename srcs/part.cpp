c47fdac2fd67f4a02944ad8d1328fde56ed73c0d
tang1304 <tanguy.gellon@gmail.com>
2024-02-14 14:22:43 +0100

    Fixed segfault when ctrl-c in a client multi chanops

--- srcs/part.cpp
+++ srcs/part.cpp
@@ -6,7 +6,7 @@
 /*   By: tgellon <tgellon@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/06 15:09:03 by tgellon           #+#    #+#             */
-/*   Updated: 2024/02/12 10:47:27 by tgellon          ###   ########lyon.fr   */
+/*   Updated: 2024/02/14 13:49:24 by tgellon          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,30 +15,39 @@
 int partCmd(int fd, vecStr &cmd, Server &serv){
 	Client		user = serv.getClientMap()[fd];
 //Modif to leave multiple channels !
+//Modif to add reason !
 	if (cmd.size() < 2){
 		sendToClient(user, ERR_NEEDMOREPARAMS(user.getName(), cmd[0]));
 		return (1);
 	}
-	std::string	chan = cmd[1];
-	itVecChan it = findIt(chan, serv.getChanList());
-	if (it == serv.getChanList().end()){
-		sendToClient(user, ERR_NOSUCHCHANNEL(user.getName(), chan));
-		return (1);
+	if (cmd.size() > 2){
+		std::string	reason;
+		for (itVecStr it = cmd.begin() + 2; it != cmd.end(); it++){
+			reason += *it;
+		}
 	}
-	bool	chanop = isItIn(user, it->getChanop());
-	if (isItIn(user, it->getUsersJoin()) == false && chanop == false){
-		sendToClient(user, ERR_NOTONCHANNEL(user.getName(), chan));
-		return (1);
+	vecStr	channels = split(cmd[1], ",");
+	for (itVecStr itChan = channels.begin(); itChan != channels.end(); itChan++){
+		itVecChan it = findIt(*itChan, serv.getChanList());
+		if (it == serv.getChanList().end()){
+			sendToClient(user, ERR_NOSUCHCHANNEL(user.getName(), *itChan));
+			return (1);
+		}
+		bool	chanop = isItIn(user, it->getChanop());
+		if (isItIn(user, it->getUsersJoin()) == false && chanop == false){
+			sendToClient(user, ERR_NOTONCHANNEL(user.getName(), *itChan));
+			return (1);
+		}
+		if (chanop == true){
+			sendToChan(*it, RPL_CMD(user.getName(), user.getUserName(), cmd[0], *itChan));
+			it->removeChanop(user);
+		}
+		else{
+			sendToChan(*it, RPL_CMD(user.getName(), user.getUserName(), cmd[0], *itChan));
+			it->removeUser(user);
+		}
+		if (it->getConnected() == 0)
+			serv.removeChan(it->getId());
 	}
-	if (chanop == true){
-		sendToChan(*it, RPL_CMD(user.getName(), user.getUserName(), cmd[0], chan));
-		it->removeChanop(user);
-	}
-	else{
-		sendToChan(*it, RPL_CMD(user.getName(), user.getUserName(), cmd[0], chan));
-		it->removeUser(user);
-	}
-	if (it->getConnected() == 0)
-		serv.removeChan(it->getId());
 	return (0);
 }
